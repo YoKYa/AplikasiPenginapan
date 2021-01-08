a81f987044f7b1e64f8b45de9820c8f5ce1589f6
danimaulana <47852543+danimaulana@users.noreply.github.com>
2021-01-08 19:43:06 +0700

    77

--- resources/views/halamanutama/header.blade.php
+++ resources/views/halamanutama/header.blade.php
@@ -44,31 +44,19 @@
         <div class="container d-flex align-items-center">
 
             <div class="logo mr-auto">
-                <h1 class="text-light"><a href="index.html"><span>RentRoom</span></a></h1>
+                <h1 class="text-light"><a href="{{ Route('halamanutama')}}"><span>RentRoom</span></a></h1>
                 <!-- Uncomment below if you prefer to use an image logo -->
                 <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
             </div>
 
             <nav class="nav-menu d-none d-lg-block">
                 <ul>
-                    <li class="active"><a href="index.html">Beranda</a></li>
-                    <li><a href="#about">Tentang</a></li>
-                    <li><a href="#team">Anggota</a></li>
-                    <li class="drop-down"><a href="">Drop Down</a>
+                    <li><a href="{{ Route('halamanutama')}}">Beranda</a></li>
+                    <li><a href="{{ Route('halamanutama')}}#about">Tentang</a></li>
+                    <li><a href="{{ Route('halamanutama')}}#team">Anggota</a></li>
+                    <li class="drop-down"><a href="">Menu</a>
                         <ul>
-                            <li><a href="#">Drop Down 1</a></li>
-                            <li class="drop-down"><a href="#">Drop Down 2</a>
-                                <ul>
-                                    <li><a href="#">Deep Drop Down 1</a></li>
-                                    <li><a href="#">Deep Drop Down 2</a></li>
-                                    <li><a href="#">Deep Drop Down 3</a></li>
-                                    <li><a href="#">Deep Drop Down 4</a></li>
-                                    <li><a href="#">Deep Drop Down 5</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="#">Drop Down 3</a></li>
-                            <li><a href="#">Drop Down 4</a></li>
-                            <li><a href="#">Drop Down 5</a></li>
+                            <li><a href="#">Login</a></li>
                         </ul>
                     </li>
                 </ul>
